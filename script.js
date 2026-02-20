2b0a06564538c85536e316deae94087f1a24312d
Vishal Patil <vishalpatilvgp2199@gmail.com>
2026-02-20 11:43:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,38 @@
-//your JS code here. If required.
 const form = document.getElementById("form");
 const ageInput = document.getElementById("age");
 const nameInput = document.getElementById("name");
 
-form.addEventLietener("submit", function(event){
-	event.preventDefault();
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
 
-	const age = ageInput.value;
-	const name = nameInput.value;
+  const age = ageInput.value;
+  const name = nameInput.value;
 
-	if (age === "" || name === "") {
-		alert("Please enter valid details.");
-		return;
-	}
+  if (age === "" || name === "") {
+    alert("Please enter valid details.");
+    return;
+  }
 
-	const checkEligibility = new Promise(function(resolve, reject) {
+  const checkEligibility = new Promise(function(resolve, reject) {
 
-		setTimeout(function () {
+    setTimeout(function() {
 
-			if (Number(age) > 18) {
-				resolve(`Welcome, ${name}. You can vote.`);
-			} else {
-				reject(`Oh sorry $[name}. You aren't old enough.`);
-			}
-		}, 4000);
-	});
+      if (Number(age) > 18) {
+        resolve(`Welcome, ${name}. You can vote.`);
+      } else {
+        reject(`Oh sorry ${name}. You aren't old enough.`);
+      }
 
-		 .then(function (message) {
-		 	alert(message);
-		 })
-		 .catch(function(error){
-			 alert(error);
-		 });
-}) 
+    }, 4000);
+
+  });
+
+  checkEligibility
+    .then(function(message) {
+      alert(message);
+    })
+    .catch(function(error) {
+      alert(error);
+    });
+
+});
